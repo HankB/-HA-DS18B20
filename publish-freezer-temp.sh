9e5144e36074813796100d331bddd5122c7ab52c
Hank Barta <hbarta@gmail.com>
2022-11-07 16:01:09 -0600

    adjust for puyallup

--- publish-freezer-temp.sh
+++ publish-freezer-temp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # meant to be run fron cron
-# */5 * * * * /home/pi/bin/publish-freezer-temp.sh
-/home/pi/bin/ds18b20-temp.py | sed -e "s/  //"|tr -d '\n'| /usr/bin/mosquitto_pub -s -t "home_automation/niwot/basement/freezer_temp" -h oak
+# */5 * * * * /home/hbarta/bin/publish-freezer-temp.sh
+/home/hbarta/bin/ds18b20-temp.py | sed -e "s/  //"|tr -d '\n'| /usr/bin/mosquitto_pub -s -t "HA/$(hostname)/kitchen/fridge_temp" -h mqtt
